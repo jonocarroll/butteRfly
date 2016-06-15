33b15d6cf3aad0b16472df45833ebe8565ac9269
Jonathan Carroll <jono@jcarroll.com.au>
2016-06-16 01:08:00 +0800

    0 check errors, plenty of other issues

--- R/theme_github.R
+++ R/theme_github.R
@@ -12,12 +12,13 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' library("ggplot2")
 #' ## get data
 #' p <- ggplot(commit_data) + geom_tile(aes(x=c.week, y=c.day)) + theme_github()
 #'
 #' ## Twitter colors
-#' p + theme_github(alt="Twitter")
+#' p + theme_github(alt="Twitter")}
 #' 
 theme_github <- function() {
   
@@ -53,7 +54,7 @@ theme_github <- function() {
 #' @export
 scale_fill_social <- function(network=c("GitHub", "Twitter", "StackOverflow"), ...) {
   # discrete_scale("fill", "social", social_pal(network = network), breaks=c(-1,0,1,5,10,20,1e5), guide=FALSE, ...)
-  discrete_scale("fill", "social", social_pal(network = network), guide=FALSE, drop=FALSE, ...)
+  discrete_scale("fill", "social", social_pal(network = network), guide = FALSE, drop = FALSE, ...)
 }
 
 #' Social networking color palettes (discrete)
@@ -70,20 +71,16 @@ scale_fill_social <- function(network=c("GitHub", "Twitter", "StackOverflow"), .
 #' @rdname scale_social
 #' @export
 #' @examples
+#' \dontrun{
 #' library(scales)
 #' show_col(social_pal("GitHub")(6))
 #' show_col(social_pal("Twitter")(6))
-#' show_col(social_pal("StackOverflow")(6))
+#' show_col(social_pal("StackOverflow")(6))}
 social_pal <- function(network=c("GitHub", "Twitter", "StackOverflow")) {
-  # function(n) {
-  # assert_that(n > 0)
-  # assert_that(n <= 6)
   function(n) {
   i <- switch(tolower(network),
-              github = c("#bbbbbb","#eeeeee","#d6e685","#1e6823","#8cc665","#44a340","#ff0000"),
-              twitter = c("#bbbbbb","#eeeeee","#9ecae1","#6baed6","#4292c6","#2171b5","#ff0000"),
+              github        = c("#bbbbbb","#eeeeee","#d6e685","#1e6823","#8cc665","#44a340","#ff0000"),
+              twitter       = c("#bbbbbb","#eeeeee","#9ecae1","#6baed6","#4292c6","#2171b5","#ff0000"),
               stackoverflow = c("#bbbbbb","#eeeeee","#fdae6b","#fd8d3c","#f16913","#d94801","#ff0000"))
-  # unname(colors[i][seq_len(n)])
   }
-  # return(i)
 }
